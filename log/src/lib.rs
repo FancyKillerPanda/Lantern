25a486bc9e820f4e723fc6c48f0e04d327081ca8
Sahil Iyer <iyersahil12@gmail.com>
2019-01-17 18:37:23 +1100

    Implemented basic logging to the console.

--- log/src/lib.rs
+++ log/src/lib.rs
@@ -1,7 +1,21 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+macro_rules! meta_log_macro {
+	($d:tt $i:ident, $msg:tt) => {
+		#[macro_export]
+		macro_rules! $i {
+			( $d ( $arg:tt )+ ) => {
+				println!($msg, format!($d ( $arg )+) )
+			};
+		}
+	};
 }
+
+meta_log_macro!($ core_trace, "[LANTERN | TRACE] {}");
+meta_log_macro!($ core_info, "[LANTERN | INFO] {}");
+meta_log_macro!($ core_warn, "[LANTERN | WARN] {}");
+meta_log_macro!($ core_error, "[LANTERN | ERROR] {}");
+meta_log_macro!($ core_fatal, "[LANTERN | FATAL] {}");
+meta_log_macro!($ trace, "[APP | TRACE] {}");
+meta_log_macro!($ info, "[APP | INFO] {}");
+meta_log_macro!($ warn, "[APP | WARN] {}");
+meta_log_macro!($ error, "[APP | ERROR] {}");
+meta_log_macro!($ fatal, "[APP | FATAL] {}");
